aa6cbc7f4c0d74ae185db1eb435a01d1e1d3ed46
Brent Hong <brenth@andrew.cmu.edu>
2019-09-04 01:40:20 -0400

    Added new tables and adding scenarios to the table

--- app/controllers/ranked_list_controller.rb
+++ app/controllers/ranked_list_controller.rb
@@ -1,4 +1,6 @@
 class RankedListController < ApplicationController
+  require 'json'
+  require 'date'
   # Randomly generate examples for ranked list
   def ranked_list
     # Get all features user selected as important
@@ -6,30 +8,61 @@ class RankedListController < ApplicationController
     @selectedFeats = selectedFeats
     # selectedFeats = Feature.request.active.added_by(3).for_user(3)
     # Size of the Ranked List 
-    rankedListSize = 3
+    rankedListSize = 5
     @rankedListScenarios = Array.new
     @rankedListExamples = Array.new
     counter = 0
+    # Since it's not a model, used raw sql... maybe should change this?
+    create_rl_sql = "insert into ranklists (participant_id, round, created_at, updated_at) values (#{current_user.id}, 1, '#{DateTime.now.strftime('%a, %d %b %Y %H:%M:%S')}', '#{DateTime.now.strftime('%a, %d %b %Y %H:%M:%S')}');"
+    ActiveRecord::Base.connection.execute(create_rl_sql)
+    new_rankedlist_id = ActiveRecord::Base.connection.execute("select ranklists.id from ranklists where participant_id = #{current_user.id} order by ranklists.id desc limit 1").values[0][0]
 
     # Randomly generate values for features 
     rankedListSize.times do
-      three_feats = selectedFeats.sample(selectedFeats.size)
-
+      all_feats = selectedFeats.sample(selectedFeats.size) # can this go outside the loop?
       if !Scenario.all.empty?
         last_id = Scenario.all.last.group_id + 1
       else
         last_id = 1
       end
-      three_feats.each do |feat|
-        if feat.data_range == nil
+
+      all_feats.each do |feat|
+        if feat.data_range == nil 
         end
-        if feat.data_range.is_categorical
+        if feat.data_range.is_categorical  
           @rankedListScenarios << Scenario.create(group_id: last_id, feature_id: feat.id, feature_value: feat.categorical_data_options.sample.option_value)
+        
         else
           @rankedListScenarios << Scenario.create(group_id: last_id, feature_id: feat.id, feature_value: ((rand(feat.data_range.lower_bound...feat.data_range.upper_bound) * 1).floor / 1.0).to_i.to_s)
         end
       end
-      @rankedListExamples << Scenario.where(group_id: last_id)
+      generated_scenario = Scenario.where(group_id: last_id)
+
+      create_scenario_sql = "insert into individual_scenarios (features, created_at, updated_at) values ('#{create_feature_json(generated_scenario)}', '#{DateTime.now.strftime('%a, %d %b %Y %H:%M:%S')}', '#{DateTime.now.strftime('%a, %d %b %Y %H:%M:%S')}');"
+      ActiveRecord::Base.connection.execute(create_scenario_sql)
+      new_scenario_id = ActiveRecord::Base.connection.execute("select individual_scenarios.id from individual_scenarios order by individual_scenarios.created_at desc limit 1").values[0][0]
+      @rankedListExamples << generated_scenario
+
+      create_rl_elem_sql = "insert into ranklist_element (ranklist_id, individual_scenario_id, model_rank, human_rank, created_at, updated_at) values (#{new_rankedlist_id}, #{new_scenario_id}, 0, 0, '#{DateTime.now.strftime('%a, %d %b %Y %H:%M:%S')}', '#{DateTime.now.strftime('%a, %d %b %Y %H:%M:%S')}');"
     end
   end
+
+  def create_feature_json(scenarios)
+    all_features = Hash.new
+    scenarios.each do |s|
+      all_features[s.feature_id] = s.feature_value
+    end
+
+    all_features.to_json
+  end
+
 end
+
+
+
+
+
+
+
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_02_195201) do
+ActiveRecord::Schema.define(version: 2019_09_04_052553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,12 @@ ActiveRecord::Schema.define(version: 2019_09_02_195201) do
     t.boolean "company", default: false
   end
 
+  create_table "individual_scenarios", force: :cascade do |t|
+    t.json "features"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "pairwise_comparisons", force: :cascade do |t|
     t.bigint "participant_id"
     t.integer "scenario_1"
@@ -108,28 +114,23 @@ ActiveRecord::Schema.define(version: 2019_09_02_195201) do
     t.string "email"
   end
 
-  create_table "rank_list_samples", force: :cascade do |t|
-    t.bigint "participant_id"
-    t.bigint "rank_list_id"
-    t.integer "round", default: 0
-    t.string "type"
-    t.bigint "feature_id"
-    t.string "feature_value"
-    t.integer "group_id"
+  create_table "ranklist_element", force: :cascade do |t|
+    t.bigint "ranklist_id"
+    t.bigint "individual_scenario_id"
+    t.integer "model_rank"
+    t.integer "human_rank"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["feature_id"], name: "index_rank_list_samples_on_feature_id"
-    t.index ["participant_id"], name: "index_rank_list_samples_on_participant_id"
-    t.index ["rank_list_id"], name: "index_rank_list_samples_on_rank_list_id"
+    t.index ["individual_scenario_id"], name: "index_ranklist_element_on_individual_scenario_id"
+    t.index ["ranklist_id"], name: "index_ranklist_element_on_ranklist_id"
   end
 
-  create_table "rank_lists", force: :cascade do |t|
+  create_table "ranklists", force: :cascade do |t|
     t.bigint "participant_id"
-    t.integer "rank"
     t.integer "round", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["participant_id"], name: "index_rank_lists_on_participant_id"
+    t.index ["participant_id"], name: "index_ranklists_on_participant_id"
   end
 
   create_table "scenarios", force: :cascade do |t|
