b4b557f115984ce7eaf95cbe6c9a89703dd4c15a
Nathan Jen <njen@andrew.cmu.edu>
2019-09-05 00:27:04 -0400

    removed drop table code to allow migrations to work

--- db/migrate/20190902195201_add_rankedlist_tables.rb
+++ db/migrate/20190902195201_add_rankedlist_tables.rb
@@ -1,7 +1,5 @@
 class AddRankedlistTables < ActiveRecord::Migration[5.2]
   def change
-    drop_table :rank_lists
-    drop_table :rank_list_samples
     
     create_table :rank_lists do |t|
       t.references :participant
