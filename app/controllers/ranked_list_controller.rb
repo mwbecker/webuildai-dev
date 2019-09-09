5e3f2fc34dcfac6e1c4bd3c590af626830500b2c
Brent Hong <brenth@andrew.cmu.edu>
2019-09-09 15:23:50 -0400

    made changes

--- app/controllers/ranked_list_controller.rb
+++ app/controllers/ranked_list_controller.rb
@@ -11,11 +11,8 @@ class RankedListController < ApplicationController
     @rankedListExamples = Array.new
     @scenarioIds = Array.new
     counter = 0
-    # Since it's not a model, used raw sql... maybe should change this?
-    create_rl_sql = "insert into ranklists (participant_id, round, created_at, updated_at) values (#{current_user.id}, 1, '#{DateTime.now.strftime('%a, %d %b %Y %H:%M:%S')}', '#{DateTime.now.strftime('%a, %d %b %Y %H:%M:%S')}');"
-    ActiveRecord::Base.connection.execute(create_rl_sql)
-    new_rankedlist_id = ActiveRecord::Base.connection.execute("select ranklists.id from ranklists where participant_id = #{current_user.id} order by ranklists.id desc limit 1").values[0][0]
 
+    @new_ranklist = Ranklist.create(participant_id: current_user.id, round: 0) # should update based on current round, should that be in sessions hash?
     all_feats = @selectedFeats.sample(@selectedFeats.size)
 
     # need to manually increment group_id
@@ -44,20 +41,15 @@ class RankedListController < ApplicationController
       # insert the scenario into the database
       @rankedListExamples << generated_scenario
       scenario_type = generated_scenario.first.feature.category
-      create_scenario_sql = "insert into individual_scenarios (features, created_at, updated_at, participant_id, category) values ('#{create_feature_json(generated_scenario)}', '#{DateTime.now.strftime('%a, %d %b %Y %H:%M:%S')}', '#{DateTime.now.strftime('%a, %d %b %Y %H:%M:%S')}', #{current_user.id}, '#{scenario_type}');"
-      ActiveRecord::Base.connection.execute(create_scenario_sql)
-
-      new_scenario = ActiveRecord::Base.connection.execute("select * from individual_scenarios order by individual_scenarios.created_at desc limit 1").values[0]
-      new_scenario_id = new_scenario[0]
-      @scenarioIds << new_scenario_id
-      # create_rl_elem_sql = "insert into ranklist_element (ranklist_id, individual_scenario_id, model_rank, human_rank, created_at, updated_at) values (#{new_rankedlist_id}, #{new_scenario_id}, 0, 0, '#{DateTime.now.strftime('%a, %d %b %Y %H:%M:%S')}', '#{DateTime.now.strftime('%a, %d %b %Y %H:%M:%S')}');"
+      new_scenario = IndividualScenario.create(participant_id: current_user.id, features: create_feature_json(generated_scenario), category: scenario_type)
+      @scenarioIds << new_scenario.id
     end
 
     # invoke python model to rank everything and insert ranklist_element table
     # model_score = `python ./model_folder/ml_model_score.py -pid #{current_user.id} -fid 1 -type request`
     return
 
-    ranklist_id = ActiveRecord::Base.connection.execute("select max(ranklist_element.ranklist_id) from ranklist_element").values[0][0]
+    ranklist_id = RanklistElement.last_ranklist.id
 
     rle_sql = "select * from ranklist_element " \
               "join individual_scenarios on individual_scenarios.id = ranklist_element.individual_scenario_id " \
@@ -65,8 +57,8 @@ class RankedListController < ApplicationController
               "order by ranklist_element.model_rank asc " \
               "limit #{@rankedListSize}"
 
-    @ranklistElems = ActiveRecord::Base.connection.execute(rle_sql).values
-
+    @ranklistElems = RanklistElement.for_ranklist(ranklist_id, @rankedListSize)
+    # @ranklistElems = ActiveRecord::Base.connection.execute(rle_sql).values
     displayElems = Array.new
     @ranklistElems.each do |elem|
       displayElem = Hash.new
--- app/models/ranklist_element.rb
+++ app/models/ranklist_element.rb
@@ -1,5 +1,22 @@
 class RanklistElement < ApplicationRecord
-  self.table_name = "ranklist_element"
+  self.table_name = "ranklist_element" # Made a mistake with the table name
   
+
+
+
+  def self.for_ranklist(ranklist_id, rl_size)
+    x = RanklistElement.joins(:individual_scenarios).where(ranklist_id: ranklist_id).order(model_rank: :asc).first(rl_size)
+    puts x
+    return x
+  end
+
+  def self.last_ranklist(participant_id)
+    if participant_id.nil?
+      return RanklistElement.order(ranklist_id: :desc).first
+    else
+      return RanklistElement.order(ranklist_id: :desc).where(participant_id: participant_id).first
+    end
+  
+  end
 
 end
