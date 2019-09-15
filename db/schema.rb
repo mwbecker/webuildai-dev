01b2d58ef28ff69a0405510cb46af51ff4eb5fe3
Brent Hong <brenth@andrew.cmu.edu>
2019-09-15 17:32:07 -0400

    Added column for units

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_05_043101) do
+ActiveRecord::Schema.define(version: 2019_09_14_205820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2019_09_05_043101) do
     t.boolean "active", default: true
     t.string "added_by"
     t.boolean "company", default: false
+    t.string "unit"
   end
 
   create_table "individual_scenarios", force: :cascade do |t|
