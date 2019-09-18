69280ccdefc2a1a1ba8a78d11426f9aa7761b3d0
Nathan Jen <njen@andrew.cmu.edu>
2019-09-18 10:28:35 -0400

    fixed typos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui/widgets/sortable
-//= require rails_sortable
 //= require activestorage
 //= require materialize
 //= require materialize-sprockets
