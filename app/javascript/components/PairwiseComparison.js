f0e3d1d01f175acbc68c38c5dbadc5fed41c626b
Brent Hong <brenth@andrew.cmu.edu>
2019-10-01 01:34:29 -0400

    fixed random ordering issue

--- app/javascript/components/PairwiseComparison.js
+++ app/javascript/components/PairwiseComparison.js
@@ -8,13 +8,13 @@ class PairwiseComparison extends React.Component {
       <div style={{width:"70%", marginLeft:"15%"}} >
         <div className="scenario_1" id="<%= pc.id %>-1" style={{width:"45%", float:"left", marginRight:"5%"}}>
           <div className="row">
-            <Scenario id={this.props.left.group_id} features={this.props.left.features} />
+            <Scenario id={this.props.left.group_id} features={this.props.left.features.sort()} />
           </div>
         </div>
 
         <div className="scenario_2" id="<%=pc.id%>-2" style={{width:"45%",float:"right"}}>
           <div className="row">
-            <Scenario id={this.props.right.group_id} features={this.props.right.features} />
+            <Scenario id={this.props.right.group_id} features={this.props.right.features.sort()} />
           </div>
         </div>
       </div>
