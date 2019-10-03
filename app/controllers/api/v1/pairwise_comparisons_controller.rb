4796b5e71675538e8a9bd2ca4c8297f42dead753
Brent Hong <brenth@andrew.cmu.edu>
2019-10-03 03:28:17 -0400

    Better scenario generation

--- app/controllers/api/v1/pairwise_comparisons_controller.rb
+++ app/controllers/api/v1/pairwise_comparisons_controller.rb
@@ -28,56 +28,25 @@ module Api
         @pairwise_comparisons = []
         # feats = Feature.active.where(category: category).added_by(current_user.id).for_user(current_user.id, category)
 
-        three_feats = feat_ids.map{|id| Feature.find(id)}
+        all_feats = feat_ids.map{|id| Feature.find(id)}
         @scenarios = []
         @num_pairs = NUM_PAIRS
         SCENS.times do
-          # three_feats = # feats.sample(feats.size)
+          # TO DO: use ScenarioGroup
+          last_id = Scenario.all.empty? ? 1 : Scenario.all.last.group_id + 1
 
-          last_id = if !Scenario.all.empty?
-                      Scenario.all.last.group_id + 1
-                    else
-                      1
-                    end
-          three_feats.each do |f|
-            data_range = f.data_range
-            if data_range.is_categorical
-              @scenarios << Scenario.create(group_id: last_id, feature_id: f.id, feature_value: f.categorical_data_options.sample.option_value)
-            else
-              if f.name.downcase['distance'] || f.name.downcase['earning'] || f.name.downcase['cancel']# checks if distance/earning/cancel is in the name
-                @scenarios << Scenario.create(group_id: last_id, feature_id: f.id, feature_value: ((rand(data_range.lower_bound..data_range.upper_bound) / 5).ceil * 5))
-              elsif f.name.downcase['rating'] && f.name.downcase['driver']
-                fval = (data_range.lower_bound+ 0.25 * (rand(((data_range.upper_bound.to_f - data_range.lower_bound.to_f)/0.25)+1)))
-                @scenarios << Scenario.create(group_id: last_id, feature_id: f.id,
-                  #feature_value: ((rand(data_range.lower_bound.to_f..data_range.upper_bound.to_f) * 4).round / 4.0).to_s)
-                  feature_value: fval)
-              elsif f.name.downcase['rating'] && f.name.downcase['customer']
-                fval = (data_range.lower_bound+ 0.1 * (rand(((data_range.upper_bound.to_f - data_range.lower_bound.to_f)/0.1)+1)))
-                @scenarios << Scenario.create(group_id: last_id, feature_id: f.id,
-                  #feature_value: ((rand(data_range.lower_bound.to_f..data_range.upper_bound.to_f) * 4).round / 4.0).to_s)
-                  feature_value: fval)
-              else
-                @scenarios << Scenario.create(group_id: last_id, feature_id: f.id, feature_value: ((rand(data_range.lower_bound...data_range.upper_bound + 1) * 1).floor / 1.0).to_i.to_s)
-              end
-            end
-          end
+          first_scenario = create_scenario(all_feats, last_id)
+          second_scenario = create_scenario(all_feats, last_id + 1)
+          @scenarios += first_scenario
+          @scenarios += second_scenario
+
+          new_pairwise = PairwiseComparison.create(participant_id: current_user.id,
+                                                   scenario_1: last_id, scenario_2: last_id + 1,
+                                                   category: category)
+          puts new_pairwise
+          @pairwise_comparisons << new_pairwise
         end
-        counter = 0
-        while counter < NUM_PAIRS
-          group_num = Scenario.all.last.group_id
-          tote = @scenarios.size / three_feats.size
-          start = group_num - tote
-          group_ind_1 = rand(start...group_num + 1)
-          group_ind_2 = rand(start...group_num + 1)
-          ind1s = Scenario.where(group_id: group_ind_1)
-          ind2s = Scenario.where(group_id: group_ind_2)
-          if (ind1s != ind2s) && ind1s.map(&:feature_id).to_set == ind2s.map(&:feature_id).to_set
-            @pairwise_comparisons << PairwiseComparison.create(
-              participant_id: current_user.id, scenario_1: group_ind_1, scenario_2: group_ind_2,
-              category: category)
-            counter += 1
-          end
-        end
+
         session[:pairwise_old_request] = @pairwise_comparisons.map{|pc| pc["id"]}
         comparisons_json = @pairwise_comparisons.map{|pc| pc.pc_to_json()}
 
@@ -96,6 +65,46 @@ module Api
         pwc.choice = choice == -1 ? nil : choice
         pwc.reason = reason
         pwc.save
+      end
+
+      private
+
+      def create_scenario(feats, group_id)
+        scenarios = Array.new
+        feats.each do |f|
+          data_range = f.data_range
+
+          if data_range.is_categorical
+            fval = f.categorical_data_options.sample.option_value
+            scenarios << Scenario.create(group_id: group_id, feature_id: f.id, 
+                                         feature_value: fval)
+          
+          else
+            if f.name.downcase['distance'] || f.name.downcase['earning'] || f.name.downcase['cancel']# checks if distance/earning/cancel is in the name
+              fval = ((rand(data_range.lower_bound..data_range.upper_bound) / 5).ceil) * 5
+              scenarios << Scenario.create(group_id: group_id, feature_id: f.id, 
+                                           feature_value: fval)
+            
+            elsif f.name.downcase['rating'] && f.name.downcase['driver']
+              fval = (data_range.lower_bound+ 0.25 * (rand(((data_range.upper_bound.to_f - data_range.lower_bound.to_f)/0.25)+1)))
+              scenarios << Scenario.create(group_id: group_id, feature_id: f.id,
+                                           feature_value: fval)
+            
+            elsif f.name.downcase['rating'] && f.name.downcase['customer']
+              fval = (data_range.lower_bound+ 0.1 * (rand(((data_range.upper_bound.to_f - data_range.lower_bound.to_f)/0.1)+1)))
+              scenarios << Scenario.create(group_id: group_id, feature_id: f.id,
+                                           feature_value: fval)
+            
+            else
+              scenarios << Scenario.create(group_id: group_id, feature_id: f.id, 
+                                           feature_value: ((rand(data_range.lower_bound...data_range.upper_bound + 1) * 1).floor / 1.0).to_i.to_s)
+            end
+          
+          end
+        end
+
+        return scenarios
+      
       end
 
     end
