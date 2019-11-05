db2c698a270fae0dce34b15ba91095ab2c0fadf3
Brent Hong <brenth@andrew.cmu.edu>
2019-11-05 17:18:23 -0500

    Removed a couple of file duplicates.

--- app/javascript/components/LoadingSpinner 2.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Spinner from '../images/loading.gif';
-
-const LoadingSpinner = ({ hide }) => {
-    return hide ? null : <img src={Spinner} />;
-}
-
-export default LoadingSpinner;
